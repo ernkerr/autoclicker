144e9acbe8e30ed2ab6f2e31d4cbb934bcecfd6c
Erin Kerr <erin.kerr17@gmail.com>
2025-07-15 15:36:39 -0700

    Initial Commit

---
+++ AutoClicker/AutoClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoClickerApp.swift
+//  AutoClicker
+//
+//  Created by Erin on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct AutoClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoClickerTests/AutoClickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AutoClickerTests.swift
+//  AutoClickerTests
+//
+//  Created by Erin on 7/15/25.
+//
+
+import Testing
+@testable import AutoClicker
+
+struct AutoClickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
